ac7a138fdf39eacf8a4f42ac8f92354035de4342
sorlros <sorlros2047@gmail.com>
2023-10-24 16:29:19 +0900

    setting

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Idea, Documents, & Plans. Unified. Welcome to <span className="underline">Sotion</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Heading
