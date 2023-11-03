1315300b2a458976283119eb64776239c9bc73d7
sorlros <sorlros2047@gmail.com>
2023-11-03 16:22:41 +0900

    Toolbar component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,36 @@
+"use client";
 
-
-const DocumentPage = () => {
-  return (
-    <div>page</div>
-  )
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
 }
 
-export default DocumentPage
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>loading</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
