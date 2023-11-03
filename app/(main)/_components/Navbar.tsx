81257aa2fb5ba274d671106291021352156a605d
sorlros <sorlros2047@gmail.com>
2023-11-03 15:43:24 +0900

    Banner component

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./Title";
+import { Banner } from "./Banner";
+import { Menu } from "./Menu";
 
 interface NavbarProps {
   isCollapse: boolean;
@@ -21,8 +23,11 @@ const Navbar = ({ isCollapse, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -43,8 +48,12 @@ const Navbar = ({ isCollapse, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
--- app/(main)/_components/TrashBox.tsx
+++ app/(main)/_components/TrashBox.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { ConfirmModal } from "@/components/modals/confirm-modal";
-import { Spinner } from "@/components/spinner";
-import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useQuery, useMutation } from "convex/react";
+import { Search, Trash, Undo } from "lucide-react";
+import { toast } from "sonner";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useMutation, useQuery } from "convex/react";
-import { Search, Trash, Undo } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
-import { toast } from "sonner";
+import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 export const TrashBox = () => {
   const router = useRouter();
@@ -37,8 +38,8 @@ export const TrashBox = () => {
 
     toast.promise(promise, {
       loading: "Restoring note...",
-      success: "Note restored",
-      error: "Failed to restore note",
+      success: "Note restored!",
+      error: " Failed to restore note.",
     });
   };
 
@@ -47,8 +48,8 @@ export const TrashBox = () => {
 
     toast.promise(promise, {
       loading: "Deleting note...",
-      success: "Note deleted",
-      error: "Failed to delete note",
+      success: "Note deleted!",
+      error: " Failed to delete note.",
     });
 
     if (params.documentId === documentId) {
@@ -72,40 +73,40 @@ export const TrashBox = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
-          placeholder="Filter by page title"
+          placeholder="Filter by page title..."
         />
-        <div className="mt-2 px-1 pb-1">
-          <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
-            No documents found
-          </p>
-          {filteredDocuments?.map((document) => (
-            <div
-              key={document._id}
-              role="button"
-              onClick={() => onClick(document._id)}
-              className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
-            >
-              <span className="truncate pl-2">{document.title}</span>
-              <div className="flex items-center">
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+          No documents found.
+        </p>
+        {filteredDocuments?.map((document) => (
+          <div
+            key={document._id}
+            role="button"
+            onClick={() => onClick(document._id)}
+            className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
+          >
+            <span className="truncate pl-2">{document.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, document._id)}
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
                 <div
-                  onClick={(e) => onRestore(e, document._id)}
                   role="button"
                   className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
-                  <Undo className="h-4 w-4 text-muted-foreground" />
+                  <Trash className="h-4 w-4 text-muted-foreground" />
                 </div>
-                <ConfirmModal onConfirm={() => onRemove(document._id)}>
-                  <div
-                    role="button"
-                    className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
-                  >
-                    <Trash className="h-4 w-4 text-muted-foreground" />
-                  </div>
-                </ConfirmModal>
-              </div>
+              </ConfirmModal>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
